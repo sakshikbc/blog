51a2720e3f1ea76ca133cce115034656ad25b86f
md-aqil <aqilali381@gmail.com>
2018-02-24 20:16:06 +0530

    ad

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -44,44 +44,33 @@
 <script src="{{ asset('js/froala_editor.min.js') }}"></script>  
 <script src="{{ asset('js/pluginController.js') }}"></script>  
 
-
-<script type="text/javascript" src="../../js/froala_editor.min.js" ></script>
-<script type="text/javascript" src="../../js/plugins/align.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/char_counter.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/code_beautifier.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/code_view.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/colors.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/draggable.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/emoticons.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/entities.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/file.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/font_size.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/font_family.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/fullscreen.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/image.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/image_manager.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/line_breaker.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/inline_style.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/link.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/lists.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/paragraph_format.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/paragraph_style.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/quick_insert.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/quote.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/table.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/save.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/url.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/video.min.js"></script>
-<script type="text/javascript" src="../../js/plugins/table.min.js"></script>
-<script>
-  $('#blog-editor').froalaEditor({
-    imageUploadURL: '/post-blog-image',
-    imageUploadParams: {
-      _token: "{{ csrf_token() }}"
-    },
-    imageManagerLoadURL: '/get-blog-images'
-  });
-</script>
+<script type="text/javascript" src="{{ asset('js/froala_editor.min.js"') }}  ></script>
+<script type="text/javascript" src="{{ asset('js/plugins/align.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/char_counter.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/code_beautifier.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/code_view.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/colors.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/draggable.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/emoticons.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/entities.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/file.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/font_size.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/font_family.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/fullscreen.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/image.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/image_manager.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/line_breaker.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/inline_style.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/link.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/lists.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/paragraph_format.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/paragraph_style.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/quick_insert.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/quote.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/table.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/save.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/url.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('js/plugins/video.min.js') }} "></script>
 
 
     </body>
